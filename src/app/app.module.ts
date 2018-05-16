3d15a2fd179217a19244bcd23d55eeb44fe2d93f
Edwin Burelo <michaeledwin@live.com.mx>
2018-05-16 04:31:00 -0500

    modificaciones menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { HeadersComponent } from './components/headers/headers.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortafolioItemComponent } from './components/portafolio-item/portafolio-item.component';
+
+import { APP_ROUTING } from './app.routes';
 
 
 @NgModule({
@@ -13,10 +16,13 @@ import { PortafolioComponent } from './components/portafolio/portafolio.componen
     AppComponent,
     HeadersComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    PortafolioItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent, PortafolioComponent, PortafolioItemComponent } from './components/index.paginas';
+
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: PortafolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'producto', component: PortafolioItemComponent },
+    { path: '**', pathMatch: 'full' , redirectTo: 'home' },
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true});
