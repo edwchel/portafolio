55e3bd31abe1833226a03bcd620aa441ed60e1e5
Edwin Burelo <michaeledwin@live.com.mx>
2018-05-19 03:54:52 -0500

    rirebase json

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,16 +6,32 @@ export class InformacionService {
 
   info: any = {};
   cargada: boolean = false;
+  cargada_personal_firebase: boolean = false;
+  equipo:any [] =[];
 
   constructor(public http:Http) {
 
+    this.carga_info();
+    this.carga_personal_firebase();
+
+   }
+
+   public carga_info(){
     this.http.get("assets/data/info.pagina.json").subscribe(
       data => {
-        console.log(data.json());
+        // console.log(data.json());
         this.cargada = true;
         this.info = data.json();
       })
+   }
 
+   public carga_personal_firebase(){
+    this.http.get("https://pageweb-angularhtml.firebaseio.com/equipo.json").subscribe(
+      data => {
+        console.log(data.json());
+        this.cargada_personal_firebase = true;
+        this.equipo = data.json();
+      })
    }
 
 }
